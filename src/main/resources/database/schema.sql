01352f8bb224efe3e2150838b2fb0a7a087f8826
Pedro Guimarães <tigreped@gmail.com>
2015-12-04 14:28:20 -0200

    #9 updating required SQL scripts for authentication database creation. Preparing for v1.2 release.

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,3 +1,7 @@
 # Structure required to accomodate authentication information
-create table api_token(auto_id serial PRIMARY KEY, token varchar(64) not null, created_at timestamp not null);
-create table api_user(auto_id serial PRIMARY KEY, email text, password varchar (64), salt varchar (32), token_id integer references api_token (auto_id));
+create table api_token(auto_id serial PRIMARY KEY, token varchar(64) unique not null, created_at timestamp not null);
+create table api_user(auto_id serial PRIMARY KEY, email text unique, password varchar (64), salt varchar (32) unique, authorized boolean default false, token_id integer references api_token (auto_id));
+insert into api_user (email, password, salt, authorized) values ('admin@sibbr.gov.br', 'a45ef5caf288ee1430a081b0d98f7f66a8b48a69b930c6c7740965875a3f4e1d', '21232f297a57a5a743894a0e4a801fc3', true);
+create index api_user_email_index on api_user (email);
+create index api_token_token_index on api_token (token);
+
